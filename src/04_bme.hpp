d0a4889b821a5ffb49fac629d6a0a9ce875fffaa
Cimanes <cimanes75@telefonica.net>
2025-05-13 11:10:38 +0200

    modified:   node-red/BME Websocket nodered.json 	modified:   platformio.ini 	modified:   src/04_bme.hpp

--- src/04_bme.hpp
+++ src/04_bme.hpp
@@ -20,7 +20,8 @@ const byte numBmeKeys = sizeof(bmeKeys) / sizeof(bmeKeys[0]);
 //======================================
 // FUNCTIONS
 //======================================
-void initBME() {      // Initialize BME280
+// Initialize BME280
+void initBME() {      
   if (!bme.begin(0x76)) {
     Serial.println(F("BME280 not found, check wiring!"));
     while (1);
@@ -28,11 +29,16 @@ void initBME() {      // Initialize BME280
   if (Debug) Serial.println(F("BME280 initialized"));
 }
 
-void readBME() {  // Read data from BME280, round to 1 decimal
+// Read data from BME280, round to 1 decimal
+void readBME() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   // Define values with *10 factor (round to one decimal)
   // Pressure is converted to mbar and then applied *10 factor
   bmeValues[0] = round(bme.readTemperature() * 10);
   bmeValues[1] = round(bme.readHumidity() * 10);
   bmeValues[2] = round(bme.readPressure() / 10);
+  timer.setTimeout(100, []() {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  });
+  
 }
-
